dbfd15d0bed5831e5a9a8b25cc11e63b15afee0c
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-18 15:12:26 +0200

    commit vehicule avant main

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -4,12 +4,56 @@ public class Vehicule {
     private int vitesseMax = 100;
     private int vitesseActuelle = 0;
 
-    public Vehicule(Marque marque, String numImmat) {
+    public Vehicule(Marque marque, String numImmat, int vitesseActuelle) {
         this.marque = marque;
         this.numImmat = numImmat;
+        this.vitesseActuelle = vitesseActuelle;
     }
 
+    public void accelerer(int n) {
+        if (vitesseActuelle + n < vitesseMax) {
+            vitesseActuelle += n;
+        }
+        else {
+            vitesseActuelle = vitesseMax;
+        }
+    }
 
+    public void freiner(int n) {
+        if (vitesseActuelle - n > 0) {
+            vitesseActuelle -= n;
+        }
+        else {
+            vitesseActuelle = 0;
+        }
+    }
+    
+
+    public Marque getMarque() {
+        return marque;
+    }
+
+    public String getNumImmat() {
+        return numImmat;
+    }
+
+    public int getVitesseActuelle() {
+        return vitesseActuelle;
+    }
+
+    public int getVitesseMax() {
+        return vitesseMax;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicule [marque=" + marque + ", numImmat=" + numImmat + ", vitesseMax=" + vitesseMax
+                + ", vitesseActuelle=" + vitesseActuelle + "]";
+    }
+
+    
+
+    
 
 
 }
