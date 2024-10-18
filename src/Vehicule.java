b2bf70684721b30e10de3433ccc601901565728f
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-18 14:41:14 +0200

    enum

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -1,4 +1,15 @@
 public class Vehicule {
-    
+    private Marque marque;
+    private String numImmat;
+    private int vitesseMax = 100;
+    private int vitesseActuelle = 0;
+
+    public Vehicule(Marque marque, String numImmat) {
+        this.marque = marque;
+        this.numImmat = numImmat;
+    }
+
+
+
 
 }
